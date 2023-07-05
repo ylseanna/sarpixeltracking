594da9d826ded4455a984300bfd919048d629f95
ylseanna <ylseannadevries@gmail.com>
2023-07-05 11:17:24 +0000

    Update FrameSeries

--- FrameSeries.py
+++ FrameSeries.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-'''Runs a series of the Pixel tracking pipelines automatically'''
+"""Runs a series of the Pixel tracking pipelines automatically"""
 
 print("\n*** Pixel Tracking Pipeline Queueing Utility ***\n")
 
@@ -8,7 +8,23 @@ import numpy as np
 import sys
 import os
 
-dem_file = "/home/data/DEM/LMI/ArcticDEM/v1/Iceland_10m.dem" 
+import argparse
+
+parser = argparse.ArgumentParser(
+    prog="ERS Pixel Tracking Pipeline Queueing Utility",
+    description="Completes a full run of the ERS pixel tracking pipelines for a series of frames (stripmap) images.",
+)
+parser.add_argument(
+    "-f",
+    "--folder",
+    dest="folder",
+    type=str,
+    required=True,
+    help="Folder to save results to.",
+)
+inps = parser.parse_args()
+
+dem_file = "/home/data/DEM/LMI/ArcticDEM/v1/Iceland_10m.dem"
 
 frames = np.loadtxt("frames.txt", dtype=str)
 
@@ -16,15 +32,18 @@ if len(frames) < 2:
     print("Insufficient frames provided")
     sys.exit()
 else:
-    print(f"{len(frames)} frames provided to compute, {len(frames) - 1} pair(s)y")
+    print(f"{len(frames)} frames provided to compute, {len(frames) - 1} pair(s)")
     for i in range(len(frames) - 1):
-        print(frames[i]+" -> "+frames[i + 1])
+        print(frames[i] + " -> " + frames[i + 1])
 
-response = input('\nRun the pipeline for this series of frames? [y, n]:')
-print(response)
+response = input("\nRun the pipeline for this series of frames? [y, n]:")
 
-for i in range(len(frames) - 1):
-        command = f"./PixelTrackingERS.py -f1 {frames[i]} -f2 {frames[i + 1]} -d {dem_file} -dest ~/PixelTrackingRuns/RUN20230704 --ignore-ampcor"
+if response == "y":
+    print("Starting series")
+    for i in range(len(frames) - 1):
+        command = f"./PixelTrackingERS.py -f1 {frames[i]} -f2 {frames[i + 1]} -d {dem_file} -dest ~/PixelTrackingRuns/{inps.folder} --ignore-ampcor"
 
-        print("\nRun #"+str(i)+":\n")
+        print("\nRun #" + str(i) + ":\n")
         os.system(command)
+else:
+    print("Aborted")
